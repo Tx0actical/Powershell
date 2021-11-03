100e795f1d59fe0358550ae3024f669d84baa061
Amit Panghal <88306361+0xNeutron@users.noreply.github.com>
2021-11-03 10:14:28 +0530

    Add files via upload

---
+++ Backup.ps1
@@ -0,0 +1,3 @@
+$date = Get-Date -format "yyy-mm-dd"
+Compress-Archive -Path './app' -CompressionLevel 'Fastest' -DestinationPath "./backup-$date"
+Write-Host "Created backup at $('./backup' + $date).zip"
